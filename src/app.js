5c6ad888b43e9cb26f98ff48a56f5841ab83fa38
BNG <bng1@BNGs-MacBook-Air.local>
2026-05-15 12:45:43 +0530

    fix test mismatch

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Updated Version 🚀");
+  res.send("CI/CD Pipeline Working 🚀");
 });
 
 if (require.main === module) {
